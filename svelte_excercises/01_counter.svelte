8bdb22068c4702d10c2415cd2f4617bb0e2251be
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-08-16 10:04:28 +0200

    Initial commit - Mirrored from my site, file: svelte_excercises/01_counter.svelte

---
+++ svelte_excercises/01_counter.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let name: String = 'world';
+	let count: int = 0;
+
+	function addOne(){
+		count++;
+	}
+</script>
+
+<h1>Hello {name}!</h1>
+<button on:click={addOne}>Clicked {count} times!</button>
+{#if count > 0}
+	<p>Don't be lazy! Click it harder!</p>
+{/if}
