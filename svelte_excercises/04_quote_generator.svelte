9a5f0d9a72c163567ade9bd1511cfe53c9cd4bf1
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-08-16 10:04:49 +0200

    Initial commit - Mirrored from my site, file: svelte_excercises/04_quote_generator.svelte

---
+++ svelte_excercises/04_quote_generator.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	const quotes_start:String =["The","A","One"];
+	const quotes_subject: String=["lion","human","small dog","dragon","little pig"];
+	const quotes_smth: String=["never","always","only"];
+	const quotes_what: String=["rapes","barks","roars","kills","drinks","defeats"];
+	const quotes_second_subject: String=["the small dog","a glass of water","its kind","the king","the traitors","the wild boar","himself","itself","herself"];
+	const quotes_end: String=["when it barks.","after the fight.","before the dark.","before the light.","in the jungle.","on the rails.","in its throne."];
+
+	function getRandomInt(max: int): int {
+		return Math.floor(Math.random() * max);
+	}
+
+	function generateQuote(): String {
+		return quotes_start[getRandomInt(quotes_start.length)]+" "+
+			quotes_subject[getRandomInt(quotes_subject.length)]+" "+
+			quotes_smth[getRandomInt(quotes_smth.length)]+" "+
+			quotes_what[getRandomInt(quotes_what.length)]+" "+
+			quotes_second_subject[getRandomInt(quotes_second_subject.length)]+" "+
+			quotes_end[getRandomInt(quotes_end.length)];
+	}
+	let curQuote:String="";
+	function storeQuote(){
+		curQuote=generateQuote();
+	}
+	
+</script>
+
+<h1>Random quote generator!</h1>
+<button on:click={storeQuote}>Click me!</button>
+<i>{curQuote}</i>
